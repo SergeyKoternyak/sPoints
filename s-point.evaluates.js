fd58a50cd7b5a068135562b659784e50aa1cf4ea
Sergey Koternyak <sergeykoternyak@gmail.com>
2017-09-19 19:05:11 +0300

    evaluates functions was added

---
+++ s-point.evaluates.js
@@ -0,0 +1,40 @@
+module.exports = {
+    getOffsetTop: $el => parseInt($el.getBoundingClientRect().top + pageYOffset),
+    
+    createByParts: (device, quantityParts = 1) => {
+        const sectionHeight = document.body.scrollHeight / quantityParts;   
+        const sections = [];
+        let topPoint = 0;
+
+        for (let i = 0; i < quantityParts; i++) {
+            sections.push({
+                top: topPoint,
+                height: sectionHeight * device.ppi
+            });
+
+            topPoint += sectionHeight * device.ppi;
+        }
+
+        return sections;
+    },
+
+    createByPoints: (device, getOffsetTop) => {
+        const scrPoints = [...document.querySelectorAll('[scr-point]')];
+        
+        return scrPoints.map((point, i, arr) => {
+            const nextPointOffset = (i === arr.length - 1) ? document.body.scrollHeight * device.ppi : getOffsetTop(arr[i + 1]);
+
+            return {
+                top: getOffsetTop(point),
+                height: nextPointOffset - getOffsetTop(point)
+            };
+        });
+    },
+
+    setMobileScale: value => {
+        const bodyStyle = document.body.style;
+        
+        bodyStyle.transform = `scale(${value})`;
+        bodyStyle.transformOrigin = '0% 0% 0px';
+    }
+}
